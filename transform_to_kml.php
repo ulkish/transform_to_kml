3a3f286e02f84d2324c8bb28d1bb3e9acd1dcf42
ulkish <dev.hugomoran@gmail.com>
2019-05-07 15:54:07 -0300

    Script gives desired output

--- transform_to_kml.php
+++ transform_to_kml.php
@@ -35,28 +35,95 @@ function get_schools_from_file()
 	return $schools_array;
 }
 
-function transform_pin_to_placemark()
+function transform_pins_to_placemarks()
 {
 	$schools = get_schools_from_file();
+
+	$all_placemarks = "";
+
 	foreach($schools as $school)
 	{
-		echo "A title: " . $school[0] . " and lat: " . $school[1] . " . ";
+		$title = $school[0];
+		$latitude = $school[1];
+		$longitude = $school[2];
+		$description = $school[3];
+
+		$format = "
+	  <Placemark>
+		<name>%s</name>
+		<description>%s</description>
+		<styleUrl>#icon-1899-0288D1</styleUrl>
+		<Point>
+		  <coordinates>
+			%f,%f
+		  </coordinates>
+		</Point>
+	  </Placemark>";
+		$all_placemarks .= sprintf($format, $title, $description, $longitude, $latitude );
 	}
+
+	return $all_placemarks;
 }
 
-transform_pin_to_placemark();
+function create_output_file()
+{
+	$all_placemarks = transform_pins_to_placemarks();
 
+	$start_of_output_file =
+'<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+  <Document>
+    <name>My map</name>
+    <description>My description for a map.</description>
+    <Style id="icon-1899-0288D1-normal">
+      <IconStyle>
+        <scale>1</scale>
+        <Icon>
+          <href>images/icon-1.png</href>
+        </Icon>
+        <hotSpot x="32" xunits="pixels" y="64" yunits="insetPixels"/>
+      </IconStyle>
+      <LabelStyle>
+        <scale>0</scale>
+      </LabelStyle>
+    </Style>
+    <Style id="icon-1899-0288D1-highlight">
+      <IconStyle>
+        <scale>1</scale>
+        <Icon>
+          <href>images/icon-1.png</href>
+        </Icon>
+        <hotSpot x="32" xunits="pixels" y="64" yunits="insetPixels"/>
+      </IconStyle>
+      <LabelStyle>
+        <scale>1</scale>
+      </LabelStyle>
+    </Style>
+    <StyleMap id="icon-1899-0288D1">
+      <Pair>
+        <key>normal</key>
+        <styleUrl>#icon-1899-0288D1-normal</styleUrl>
+      </Pair>
+      <Pair>
+        <key>highlight</key>
+        <styleUrl>#icon-1899-0288D1-highlight</styleUrl>
+      </Pair>
+    </StyleMap>
+    <Folder>
+      <name>My untitled layer</name>';
 
-//print_r($school_list);
-// A placemark looks like this:
-//
- //  <Placemark>
-	// <name>{$title}</name>
-	// <description>{$description}</description>
-	// <styleUrl>#icon-1899-0288D1</styleUrl>
-	// <Point>
-	//   <coordinates>
-	// 	{$longitude},{$latitude}
-	//   </coordinates>
-	// </Point>
- //  </Placemark>
+	$end_of_output_file = '
+    </Folder>
+  </Document>
+</kml>';
+
+	$complete_output_file = $start_of_output_file .= $all_placemarks .= $end_of_output_file;
+
+	return $complete_output_file;
+}
+
+$new_file = 'map.kml';
+
+$output_file = create_output_file();
+
+file_put_contents($new_file, $output_file);
