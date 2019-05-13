14c76c4aa05fe5d2b8e74d740326e7e22017b0aa
ulkish <dev.hugomoran@gmail.com>
2019-05-13 10:35:19 -0300

    Added .gitignore

--- transform_to_kml.php
+++ transform_to_kml.php
@@ -1,43 +1,90 @@
 <?php
 
-// First regex:
-// [et_pb_map_pin_extended](get this)[/et_pb_map_pin_extended]
-
-// Second regex:
-// (title)(pin_address)(pin_address_lat)(pin_address_lng)( # of schools served: (n))(src)*
-
-$file = file_get_contents('schools.txt');
-$first_pin_regex = '/\[et_pb_map_pin_extended(.*?)\/et_pb_map_pin_extended\]/ms';
 
 
-preg_match_all($first_pin_regex, $file, $matches);
-
-$all_matches = implode(' ', $matches[1]);
-
-echo $all_matches;
-
-// TODO:
-//	1. Ask the user for the file name.
-//	2. Create or ask for map name and description.
-//	3. Return a file ready to be imported.
 function get_schools_from_file()
 {
 	$file = file_get_contents('schools.txt');
 
-	//$all_pins_from_file = '';
-	//
-	// Matches contents of title, latitude, longitude and description in that order.
-	$pin_regex = '/title="([^"]+)" pin_address="[^"]+" pin_address_lat="([^"]+)" pin_address_lng="([^"]+)"(?:(?!# of).)*(# of schools served: [0-9]*)/';
-	preg_match_all($pin_regex, $file, $matches);
+	$complete_pin_regex = '/\[et_pb_map_pin_extended(.*?)\[\/et_pb_map_pin_extended\]/ms';
+	preg_match_all($complete_pin_regex, $file, $complete_pin_matches);
+
+	// Creating arrays for storing pin attributes.
+	$pin_titles = array();
+	$pin_latitudes = array();
+	$pin_longitudes= array();
+	$pin_descriptions = array();
 
 
-	$schools_sum = count($matches[0]);
+	foreach($complete_pin_matches[0] as $complete_pin)
+	{
+		// Extracts title, latitude, and longitude from every pin.
+		$pin_attributes_regex = '/title="([^"]+)" pin_address="[^"]+" pin_address_lat="([^"]+)" pin_address_lng="([^"]+)"/';
+		preg_match($pin_attributes_regex, $complete_pin, $att_matches);
+		// Pushing attributes to their corresponding arrays
+		array_push($pin_titles, $att_matches[1]);
+		array_push($pin_latitudes, $att_matches[2]);
+		array_push($pin_longitudes, $att_matches[3]);
+
+		// Extract descriptions from every pin.
+		$description_regex = '/(?:(?!06"]).)*06"\](.*?)[<\[]/ms';
+		preg_match($description_regex, $complete_pin, $desc_matches);
+		$pin_description = $desc_matches[1] . '<br>';
+
+		// Extracts images from every pin.
+		$pin_imgs_regex = '/ src="([^"]+)"/ms';
+		preg_match_all($pin_imgs_regex, $complete_pin, $img_matches);
+		$images_as_text = '';
+		foreach($img_matches[1] as $image)
+		{
+			switch($image) {
+				case 'http://vh1savethemusic.dev9.tipit.net/wp-content/uploads/2018/03/generalmusic_intromusic_iconv3.png':
+					$images_as_text .= '• Intro To Music Grant <br>';
+					break;
+				case 'http://vh1savethemusic.dev9.tipit.net/wp-content/uploads/2018/03/Band_iconv3.png':
+					$images_as_text .= '• Band Core Grant <br>';
+					break;
+				case 'http://vh1savethemusic.dev9.tipit.net/wp-content/uploads/2018/03/strings_iconv3.png':
+					$images_as_text .= '• Strings Core Grant <br>';
+					break;
+				case 'http://vh1savethemusic.dev9.tipit.net/wp-content/uploads/2018/03/mariachi_iconv3.png':
+					$images_as_text .= '• Mariachi Core Grant <br>';
+					break;
+				case 'http://vh1savethemusic.dev9.tipit.net/wp-content/uploads/2018/03/guitarLabiconv3.png':
+					$images_as_text .= '• Guitar Lab <br>';
+					break;
+				case 'http://vh1savethemusic.dev9.tipit.net/wp-content/uploads/2018/03/KeyboardLabiconv3.png':
+					$images_as_text .= '• Keyboard Lab <br>';
+					break;
+				case 'http://vh1savethemusic.dev9.tipit.net/wp-content/uploads/2018/03/piano_keyskids_iconv3.png':
+					$images_as_text .= '• Keys + Kids Piano Grant <br>';
+					break;
+				default:
+					$images_as_text .= '';
+			}
+		}
+		// Cleaning, formatting and finally appending image names to pin descriptions.
+		$cleaned_pin_description = preg_replace( "/\r|\n/", " ", $pin_description);
+
+		$cleaned_pin_description .= $images_as_text;
+
+		$formatted_pin_description = '<![CDATA[' . $cleaned_pin_description . ']]>';
+		array_push($pin_descriptions, $formatted_pin_description);
+	}
+
+
+	// Generating the keys for the school multidimentional array creation.
+	$schools_sum = count($complete_pin_matches[0]);
 	$schools_sum_prepared = --$schools_sum;
 	$schools_sum_keys = range(0, $schools_sum_prepared);
 
-	// Create an array with every school as an object
-	$schools_array = array();
-	$values = array($matches[1], $matches[2], $matches[3], $matches[4]);
+	// Create a multidimentional array with every school as an object.
+	$schools_group = array();
+	$values = array($pin_titles,
+					$pin_latitudes,
+					$pin_longitudes,
+					$pin_descriptions,
+					);
 	foreach($schools_sum_keys as $index => $key)
 	{
 		$school = array();
@@ -45,13 +92,13 @@ function get_schools_from_file()
 		{
 			$school[] = $value[$index];
 		}
-		$schools_array[$key]  = $school;
+		$schools_group[$key]  = $school;
 	}
 
-	//print_r($schools_array);
-	return $schools_array;
-}
 
+	return $schools_group;
+}
+get_schools_from_file();
 function transform_pins_to_placemarks()
 {
 	$schools = get_schools_from_file();
@@ -60,6 +107,7 @@ function transform_pins_to_placemarks()
 
 	foreach($schools as $school)
 	{
+		// Check if the school needs a "school with images" format.
 		$title = $school[0];
 		$latitude = $school[1];
 		$longitude = $school[2];
@@ -148,5 +196,5 @@ function init()
 	file_put_contents($new_file, $output_file);
 }
 
-//init();
+init();
 
